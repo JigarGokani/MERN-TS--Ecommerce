06a85c9b2484c4f4958cbd7956e78864f7961899
Jigar-gokani <jigar.gokani@somaiya.edu>
2024-01-11 20:03:23 +0530

    multer integrated

---
+++ ecommerce-backend/dist/middlewares/isAuth.js
@@ -0,0 +1,18 @@
+import { User } from "../models/user.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "./error.js";
+export const isAdmin = TryCatch(async (req, res, next) => {
+    const id = req.query.id || req.params.id;
+    console.log(id);
+    if (!id) {
+        return next(new ErrorHandler("You are not logedin user", 402));
+    }
+    const user = await User.findById(id);
+    if (!user) {
+        return next(new ErrorHandler("Invalid ID", 402));
+    }
+    if (user.role !== "admin") {
+        return next(new ErrorHandler("You don't have any rights(Admin protected route)", 402));
+    }
+    next();
+});
--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -1,14 +1,15 @@
 import  express  from "express";
 import { deleteUser, getAllUser, getUser, newUser } from "../controllers/user.js";
+import { isAdmin } from "../middlewares/isAuth.js";
 
 const app = express.Router();
 
 
 app.post("/new",newUser);
-app.get("/all",getAllUser);
+app.get("/all",isAdmin,getAllUser);
 
 app.get("/:id",getUser);
-app.delete("/:id",deleteUser);
+app.delete("/:id",isAdmin,deleteUser);
 
 
 
